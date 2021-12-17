6bd1aab3515f98aef1b0d5415b7e7b4f5e4468e4
Esteban Burgoa <95539851+aburgoaor@users.noreply.github.com>
2021-12-16 22:02:40 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -5,6 +5,17 @@
 		wp_enqueue_style('style', get_stylesheet_uri());
 		wp_enqueue_script('jquery');
 		wp_enqueue_script('bootstrapjs', get_stylesheet_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '5.1.3', true);
+		wp_enqueue_script('fontawesome', 'https://yourkitawesomehere.js');
 	}
 
 	add_action('wp_enqueue_scripts', 'theme_style');
+
+	//Add thumbnails
+	add_theme_support('post-thumbnails');
+
+	//Add menus
+	register_nav_menus(array(
+		'main_menu' => __('Main Menu','theme_style'),
+		'sencondary_menu' => __('Secondary Menu','theme_style')
+	));
+	
